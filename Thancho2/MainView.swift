7ca9d05d5d841964d3afcdc16c2ad4df5b7446d8
Jenna <7373qkrwlgml@gmail.com>
2025-04-18 17:31:35 +0900

    feat. main view까지 생성

---
+++ Thancho2/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Thancho2
+//
+//  Created by 박지희 on 4/18/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
